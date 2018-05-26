d0fcc73ec60b02013ab1adb499140ae3c4b9eede
mircea <mircea.negoita13@gmail.com>
2018-05-26 22:19:04 +0300

    Restyled admin dashboard + added dummy text

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -9,7 +9,21 @@
 @endsection
 
 @section('content')
-    <h1>It works :) </h1>
+    <div class="row" style="padding-top:40px;">
+        <div class="container-fluid">
+            <div class="box">
+                <div class="box-header">
+                    <h4 class="heading-title">Dashboard</h4>
+                </div>
+                <hr>
+                <div class="box-content">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum perspiciatis quia consequatur est eum? Illo aliquam harum iusto quibusdam id ut vel saepe, nisi veritatis voluptatum rerum alias provident dolores quo beatae incidunt porro accusamus voluptas quis laborum? Ipsam quam enim nam inventore expedita dolor aspernatur, vero exercitationem molestiae rerum ipsa, delectus recusandae distinctio, sequi officiis dicta ratione voluptatum quod. Natus, architecto cumque repudiandae modi molestiae culpa reprehenderit impedit rem totam quod doloremque ea a amet maxime ex blanditiis non beatae facere voluptate aut odit. Quas error officiis esse similique quos fuga porro, exercitationem, eveniet veniam asperiores cupiditate blanditiis facere?</p>
+                </div>
+                <hr>
+                
+            </div>
+        </div>
+    </div>
 @endsection
 
 @section('page_scripts')
