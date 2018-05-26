f8d3a0a56bb73d8f3e904d0373b9e19fb9ac6cad
mircea <mircea.negoita13@gmail.com>
2018-05-26 22:18:15 +0300

    Restyled admin messages

--- resources/views/admin/messages/index.blade.php
+++ resources/views/admin/messages/index.blade.php
@@ -6,35 +6,38 @@
 
 @section('content')
     <div class="row" style="padding-top:40px;">
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <h1>Messages</h1>
-            </div>
-            <div class="panel-body">
-                @if ($messages->count())
-                    <table class="table table-bordered">
-                        <thead>
-                            <th>ID</th>
-                            <th>Name</th>
-                            <th>Email</th>
-                            <th>Subject</th>
-                            <th>Show</th>
-                        </thead>
-                        <tbody>
-                            @foreach ($messages as $message)
-                                <tr>
-                                    <td>{{ $message->id }}</td>
-                                    <td>{{ $message->name }}</td>
-                                    <td>{{ $message->email }}</td>
-                                    <td>{{ $message->subject }}</td>
-                                    <td><a class="btn btn-info" href="{{ route('messages.show', ['id' => $message->id]) }}"><i class="fa fa-eye"></i>Show</a></td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                @else
-                    <p>No messages :( , forever alone</p>
-                @endif
+        <div class="container-fluid">
+            <div class="box">
+                <div class="box-heading">
+                    <h4 class="heading-title">Messages</h4>
+                </div>
+                <hr>
+                <div class="box-content">
+                    @if ($messages->count())
+                        <table class="table table-bordered">
+                            <thead>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Subject</th>
+                                <th>Show</th>
+                            </thead>
+                            <tbody>
+                                @foreach ($messages as $message)
+                                    <tr>
+                                        <td>{{ $message->id }}</td>
+                                        <td>{{ $message->name }}</td>
+                                        <td>{{ $message->email }}</td>
+                                        <td>{{ $message->subject }}</td>
+                                        <td><a class="btn btn-info" href="{{ route('messages.show', ['id' => $message->id]) }}"><i class="fa fa-eye"></i>Show</a></td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    @else
+                        <p>No messages :( , forever alone</p>
+                    @endif
+                </div>
             </div>
         </div>
     </div>
--- resources/views/admin/messages/show.blade.php
+++ resources/views/admin/messages/show.blade.php
@@ -6,20 +6,22 @@
 
 @section('content')
     <div class="row" style="padding-top:40px;">
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <div class="row">
-                    <div class="col-lg-12 col-md-12 col-sm-12">
+        <div class="container-fluid">
+            <div class="box">
+                <div class="box-heading">
+                    <div class="row">
                         <div class="col-lg-12 col-md-12 col-sm-12">
-                            <h3>Message From {{ $message->email }}</h3>
+                            <div class="col-lg-12 col-md-12 col-sm-12">
+                                <h4 class="heading-title">Message From {{ $message->email }}</h4>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-    
-            <div class="panel-body">
-                <h4>Subject : {{ $message->subject }}</h4>
-                <p>Message : {{ $message->message }}</p>
+                <hr>
+                <div class="box-content">
+                    <h4>Subject : {{ $message->subject }}</h4>
+                    <p>Message : {{ $message->message }}</p>
+                </div>
             </div>
         </div>
     </div>
